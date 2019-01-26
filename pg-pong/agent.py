8447fd814d26c2d15cbd783ad6c1b2e715025763
Lucas Emery <lucas.emery@hotmail.com>
2019-01-26 01:02:04 -0300

    Added bias to model

--- pg-pong/agent.py
+++ pg-pong/agent.py
@@ -8,11 +8,11 @@ from time import sleep
 
 H = 50
 D = 25600
-learn_rate = 1e-3
+learn_rate = 1e-4
 discount_factor = 0.99
 
 resume = True
-render = False
+render = True
 
 
 def main():
@@ -21,12 +21,16 @@ def main():
 
     if resume:
         with open("model.p", "rb") as f:
-            W = pickle.load(f)
+            W, B = pickle.load(f)
     else:
         w1 = np.random.rand(H, D) / math.sqrt(D)    # Xavier init
-        w2 = np.random.rand(1, H) / math.sqrt(H)
+        w2 = (np.random.rand(1, H) - 0.5) / math.sqrt(H)    # Deducted Uniform(0, 1) mean to eliminate tendency to go UP
+                                                            # This is possible because negative values have no negative
+        b1 = np.random.rand(H)                              # effects in this layer (sigmoid activation)
+        b2 = np.zeros(1)
 
         W = [w1, w2]
+        B = [b1, b2]
 
     ep = 0
     while True:
@@ -40,12 +44,11 @@ def main():
                 env.render()
                 sleep(1/60)
 
-
             cur_x = preprocess(observation)
             x = cur_x + prev_x if prev_x is not None else np.zeros_like(cur_x)
             prev_x = cur_x
 
-            prob = forward_pass(x, W, xs, hs)
+            prob = forward_pass(x, W, B, xs, hs)
 
             action = 4 if np.random.uniform() < prob else 3
 
@@ -66,12 +69,12 @@ def main():
         ds_log_policy = np.vstack(ds_log_prob)
         rs = np.vstack(rs)
 
-        train(W, xs, hs, ds_log_policy, rs)
+        train(W, B, xs, hs, ds_log_policy, rs)
 
         ep += 1
         if ep % 100 == 0:
             with open("model.p", "wb") as f:
-                pickle.dump(W, f)
+                pickle.dump([W, B], f)
 
         observation = env.reset()
 
@@ -87,19 +90,19 @@ def preprocess(observation):
     return x
 
 
-def forward_pass(x, w, xs, hs):
+def forward_pass(x, w, b, xs, hs):
     xs.append(x)
 
-    h = np.dot(w[0], x)
+    h = np.dot(w[0], x) + b[0]
     h[h < 0] = 0
 
     hs.append(h)
 
-    log_prob = np.dot(w[1], h)
+    log_prob = np.dot(w[1], h) + b[1]
     return sigmoid(log_prob)
 
 
-def train(w, xs, hs, ds_log_prob, rs):
+def train(w, b, xs, hs, ds_log_prob, rs):
     calculate_rewards(rs)
 
     rs -= np.mean(rs)
@@ -109,21 +112,26 @@ def train(w, xs, hs, ds_log_prob, rs):
 
     ds_log_prob *= rs     # Modulate gradient with reward
 
-    dw = backward_pass(w, xs, hs, ds_log_prob)
+    dw, db = backward_pass(w, b, xs, hs, ds_log_prob)
 
     for i in range(len(w)):
         w[i] += learn_rate * dw[i]
 
+    for i in range(len(b)):
+        b[i] += learn_rate * db[i]
 
-def backward_pass(w, xs, hs, ds_log_prob):
+
+def backward_pass(w, b, xs, hs, ds_log_prob):
     dw1 = np.dot(ds_log_prob.T, hs)
+    db1 = np.sum(ds_log_prob, axis=0)
 
     dhs = np.dot(ds_log_prob, w[1])
     dhs[hs == 0] = 0
 
     dw0 = np.dot(dhs.T, xs)
+    db0 = np.sum(dhs, axis=0)
 
-    return [dw0, dw1]
+    return [dw0, dw1], [db0, db1]
 
 
 def calculate_rewards(rs):
